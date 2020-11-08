978a5ec6d7c480da1ba885879e40a65ff231f300
R. A. Souza <alves.wm@gmail.com>
2020-11-08 00:55:07 -0300

    function guard

--- enum.exs
+++ enum.exs
@@ -2,7 +2,8 @@ defmodule My.Enum do
   def head([]), do: nil
   def head([head | _]), do: head
 
-  def apply(elem, f) do
+  # First Class function
+  def apply(elem, f) when is_function(f) do
     f.(elem)
   end
 end
